551d35649b97bdb19b7e04eef33e766928c3936c
Abner Garbin <abner.batysta151@gmail.com>
2024-11-22 06:45:42 -0300

    Implementando findById

--- src/src/application/Program.java
+++ src/src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +12,8 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
     }
